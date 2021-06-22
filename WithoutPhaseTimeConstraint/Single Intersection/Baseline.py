1874c116eeeaae9f5d99a0e0d66bf0fc79be8d1a
washim-uddin-mondal <74007561+washim-uddin-mondal@users.noreply.github.com>
2021-06-22 00:20:22 -0400

    Add files via upload

--- WithoutPhaseTimeConstraint/Single Intersection/Baseline.py
+++ WithoutPhaseTimeConstraint/Single Intersection/Baseline.py
@@ -5,12 +5,12 @@ import torch
 
 def evaluate(args):
     env = args.env
-    eval_queue = 0
+    MeanQ = 0
 
     for iter_count in range(args.run_eval):
         curr_phase = torch.randint(0, 2, [1])
         env.simulate(curr_phase)
         next_state = env.states.clone()
-        eval_queue += (torch.sum(next_state) - eval_queue) / (iter_count + 1)
+        MeanQ += (torch.sum(next_state) - MeanQ) / (iter_count + 1)
 
-    print(f"Baseline: Mean queue length after evaluation is: {eval_queue}")
+    print(f"Baseline: Mean queue length after evaluation is: {MeanQ}")
